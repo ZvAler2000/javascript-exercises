31e3bf59008b0d2d7f7ff3436b3aea138355319e
ZvAler2000 <aleric.zvonimir@gmail.com>
2025-02-07 03:22:33 +0100

    Finished second exercise in JavaScript Exercises, Repeat Strings.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let output = "";
+    
+    switch(true) {
+        case num > 0:
+            for(let i = 0; i < num; i++) {
+                output += string;
+            }
+            break;
+        case num == 0:
+            break;
+        case num < 0:
+            output = "ERROR";
+            break;
+    }
 
+    return output;
 };
 
 // Do not edit below this line
