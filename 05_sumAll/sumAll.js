32beebd70f385462d311f9b384ddef7d88c7a31f
ZvAler2000 <aleric.zvonimir@gmail.com>
2025-02-09 04:21:00 +0100

    Finished fifth exercise in Javascript Exercises, Sum All.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let output = 0;
+    let temp;
 
+    if(firstNum < 0 || secondNum < 0 || !Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return "ERROR";
+    }
+
+    if(secondNum < firstNum) {
+        temp = secondNum;
+        secondNum = firstNum;
+        firstNum = temp;
+    }
+
+    for(let i = firstNum; i <= secondNum; i++) {
+        output += i;
+    }
+
+    return output;
 };
 
 // Do not edit below this line
