e642e935745a195b2c3e75bae70cff95f2e68c2d
ZvAler2000 <aleric.zvonimir@gmail.com>
2025-02-14 20:41:09 +0100

    Finished twelveth and final exercise in Javascript Exercises, Find the Oldest.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(input) {
+    let ageArray = input.map((person) => {
+        if(person.yearOfDeath == undefined) {
+            return new Date().getFullYear() - person.yearOfBirth;
+        }else {
+            return person.yearOfDeath - person.yearOfBirth;
+        }
+    });
+    
+    return input.splice(ageArray.indexOf(Math.max(...ageArray)), 1)[0];
 };
 
 // Do not edit below this line
