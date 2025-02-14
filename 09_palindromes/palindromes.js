14bb1b9b2d7d4cd6dd8be363f5e88cb2026e8a7c
ZvAler2000 <aleric.zvonimir@gmail.com>
2025-02-14 19:27:00 +0100

    Finished ninth exercise in Javascript Exercises, Palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    input = input.replace(/[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    input = input.toLowerCase();
+    let reverse = input.split("").reverse().join("");
 
+    return reverse === input;
 };
 
 // Do not edit below this line
