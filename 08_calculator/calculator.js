d13234b5ed61371b3e3fedcfa8689fd5812a7bc3
ZvAler2000 <aleric.zvonimir@gmail.com>
2025-02-14 18:28:30 +0100

    Finished eighth exercise in Javascript Exercises, Calculator.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(number, toThePower) {
+	if(toThePower > 0) {
+    return number * power(number, --toThePower);
+  }else {
+    return 1;
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+	if(input > 0) {
+    return input * factorial(--input);
+  }else {
+    return 1;
+  }
 };
 
 // Do not edit below this line
