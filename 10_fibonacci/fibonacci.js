18a328134ffd4fa9f8f3022baabf6af41b599228
ZvAler2000 <aleric.zvonimir@gmail.com>
2025-02-14 19:45:47 +0100

    Finished tenth exercise in Javascript Exercises, Fibonacci.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let fibonacciArray = [];
 
+    if(input < 0) {
+        return "OOPS";
+    }
+
+    for(let i = 0; i <= parseInt(input); i++) {
+        if(i == 0  || i == 1) {
+            fibonacciArray[i] = i;
+        }else {
+            fibonacciArray[i] = fibonacciArray[i-2] + fibonacciArray[i-1];
+        }
+    }
+
+    return fibonacciArray[input];
 };
 
 // Do not edit below this line
